2c9c5f8e8f6300cc40649a3da34c622002d9066a
Albérico Bezerra <alberico.sp@gmail.com>
2024-11-12 14:49:50 +0000

    finaliza `endpoint`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response){
-  const result = await database.query('SELECT 1 + 1 as sum;');
-  console.log(result);
-  response.status (200).json({ chave: "são acima da média"});
+  const updatedAt = new Date().toISOString();
+
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnResult = await database.query("SHOW max_connections;");
+  const dbMaxConnValue = dbMaxConnResult.rows[0].max_connections;
+  
+  const dbName = process.env.POSTGRES_DB;
+  const dbOpenedConnResult = await database.query({
+    text:"SELECT count(*)::int cont FROM pg_stat_activity where datname = $1;",
+    values: [dbName],
+  });
+  const dbOpenedConnValue = dbOpenedConnResult.rows[0].cont;
+
+  response.status (200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: dbMaxConnValue,
+        opened_connections: dbOpenedConnValue,
+      }
+    }
+  });
 }
 
 export default status;
